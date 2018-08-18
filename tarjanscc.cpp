0a6f813316e4d7b3d606fdcb0364008adf34d568
Linkus000 <linkus@openmailbox.org>
2018-08-18 20:26:36 +0200

    Extend tarjan SCC file to also include creating an SCC graph

--- tarjanscc.cpp
+++ tarjanscc.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MX 50001
-vector<int> adj[MX];
-int dfs_num[MX], dfs_min[MX];
+#define N 50001
+vector<int> adj[N];
+int dfs_num[N], dfs_min[N];
+int wh[N];
+vector<int> adj2[N];
 vector<vector<int>> scc;
 stack<int> st;
 int it;
@@ -22,6 +24,7 @@ void tarjan(int cur){
             //elements of the scc
             cr = st.top(); st.pop();
             dfs_min[cr] = 0x7f7f7f7f;
+            wh[cr] = scc.size()-1;
             scc.back().push_back(cr);
         }while(cr != cur);
     }
@@ -36,7 +39,29 @@ int main(){
         adj[f].push_back(s);
     }
     for(int i = 1; i <= n; ++i){
-        if(!dfs_num[i])
+        if(!dfs_num[i]){
             tarjan(i);
+        }
+    }
+    int nn = scc.size();
+    bool was[n];
+    memset(was,0,n);
+    int qu[n+1];
+    int *quit = qu;
+    for(int i = 0; i < nn; ++i){
+        wh[i] = 1;
+        for(auto &x: scc[i]){
+            for(auto &j: adj[x]){
+                if(!was[wh[j]]){
+                    was[wh[j]] = 1;
+                    adj2[i].push_back(wh[j]);
+                    *++quit = wh[j];
+                }
+            }
+        }
+        wh[i] = 0;
+        while(quit != qu){
+            wh[*quit--] = 0;
+        }
     }
 }
