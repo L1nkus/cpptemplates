7cd8a71ad1f96834f10680a4c15df72956022bc2
Linkus000 <linkus@openmailbox.org>
2022-03-19 19:50:14 +0100

    Very fast min cost circulation (or can be used just as mcmf)

---
+++ sos_sub_over_subsets_supersets.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define mp make_pair
+#define all(a) begin(a),end(a)
+#define FOR(x,val,to) for(int x=(val);x<int((to));++x)
+#define FORE(x,val,to) for(auto x=(val);x<=(to);++x)
+#define FORR(x,arr) for(auto &x: arr)
+#define FORS(x,plus,arr) for(auto x = begin(arr)+(plus); x != end(arr); ++x)
+#define FORREV(x,plus,arr) for(auto x = (arr).rbegin()+(plus); x !=(arr).rend(); ++x)
+#define REE(s_) {cout<<s_<<'\n';exit(0);}
+#define GET(arr) for(auto &i: (arr)) sc(i)
+#define whatis(x) cerr << #x << " is " << (x) << endl;
+#define e1 first
+#define e2 second
+#define INF 0x7f7f7f7f
+typedef std::pair<int,int> pi;
+typedef std::vector<int> vi;
+typedef std::vector<std::string> vs;
+typedef int64_t ll;
+typedef uint64_t ull;
+#define umap unordered_map
+#define uset unordered_set
+using namespace std;
+using namespace __gnu_pbds;
+
+#ifdef ONLINE_JUDGE
+#define whatis(x) ;
+#endif
+#ifdef _WIN32
+#define getchar_unlocked() _getchar_nolock()
+#define _CRT_DISABLE_PERFCRIT_LOCKS
+#endif
+template<class L, class R> ostream& operator<<(ostream &os, map<L, R> P) { for(auto const &vv: P)os<<"("<<vv.first<<","<<vv.second<<")"; return os; }
+template<class T> ostream& operator<<(ostream &os, set<T> V) { os<<"[";for(auto const &vv:V)os<<vv<<","; os<<"]"; return os; }
+template<class T> ostream& operator<<(ostream &os, vector<T> V) { os<<"[";for(auto const &vv:V)os<<vv<<","; os<<"]"; return os; }
+template<class L, class R> ostream& operator<<(ostream &os, pair<L, R> P) { os<<"("<<P.first<<","<<P.second<<")"; return os; }
+inline int fstoi(const string &str){auto it=str.begin();bool neg=0;int num=0;if(*it=='-')neg=1;else num=*it-'0';++it;while(it<str.end()) num=num*10+(*it++-'0');if(neg)num*=-1;return num;}
+inline void getch(char &x){while(x = getchar_unlocked(), x < 33){;}}
+inline void getstr(string &str){str.clear(); char cur;while(cur=getchar_unlocked(),cur<33){;}while(cur>32){str+=cur;cur=getchar_unlocked();}}
+template<typename T> inline bool sc(T &num){ bool neg=0; int c; num=0; while(c=getchar_unlocked(),c<33){if(c == EOF) return false;} if(c=='-'){ neg=1; c=getchar_unlocked(); } for(;c>47;c=getchar_unlocked()) num=num*10+c-48; if(neg) num*=-1; return true;}template<typename T, typename ...Args> inline void sc(T &num, Args &...args){ bool neg=0; int c; num=0; while(c=getchar_unlocked(),c<33){;} if(c=='-'){ neg=1; c=getchar_unlocked(); } for(;c>47;c=getchar_unlocked()) num=num*10+c-48; if(neg) num*=-1; sc(args...); }
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; //s.find_by_order(), s.order_of_key() <- works like lower_bound
+template<typename T> using ordered_map = tree<T, int, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int N;
+    cin >> N;
+    int A[1 << N];
+    FORR(i,A)
+        cin >> i;
+    int F[1 << N]; // sum over subsets
+    int supF[1 << N]; // sum over supersets
+    int Fbitdf[1 << N][N + 1]; // sum over subsets by bit diff cnt.
+    for(int i = 0; i < 1 << N; ++i){
+        F[i] = A[i];
+        supF[i] = A[i];
+        Fbitdf[i][0] = A[i];
+    }
+    for(int i = 0;i < N; ++i) for(int mask = 0; mask < (1<<N); ++mask){
+        if(mask & (1<<i)){
+            F[mask] += F[mask^(1<<i)];
+        }
+    }
+    for(int i = 0;i < N; ++i) for(int mask = 0; mask < (1<<N); ++mask){
+        if(!(mask & (1<<i))){
+            supF[mask] += supF[mask^(1<<i)];
+        }
+    }
+    for(int i = 0;i < N; ++i) for(int mask = 0; mask < (1<<N); ++mask){
+        if(mask & (1<<i)){
+            for(int j = 1; j < N + 1; ++j)
+                Fbitdf[mask][j] += Fbitdf[mask^(1<<i)][j - 1];
+        }
+    }
+}
+
+
